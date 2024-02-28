2b519dc8d5e048d59268417b14a406afd02df7b9
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-02-28 12:53:30 +0700

    deleted unnecessary import

--- src/main/java/ru/nsu/valikov/generators/TranslationVisitor.java
+++ src/main/java/ru/nsu/valikov/generators/TranslationVisitor.java
@@ -7,7 +7,6 @@ import antlr.ClojureParser.ExpressionsContext;
 import antlr.ClojureParser.IfContext;
 import antlr.ClojureParser.ProgramContext;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
