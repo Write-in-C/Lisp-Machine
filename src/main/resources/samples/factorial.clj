59b4ae7955ef3725dc766cbb66d8e5f1b6c53b4f
Nikolay <58514394+nocarend@users.noreply.github.com>
2024-03-13 01:47:48 +0700

    typehints and new scheme
    короче done is better than nothing
    даже не perfect

--- src/main/resources/samples/factorial.clj
+++ src/main/resources/samples/factorial.clj
@@ -1,5 +1,5 @@
 (ns factorial)
-(defn factorial [n] // можно писать аннотации для типов, типо хинтов [^int n]
+(defn factorial ^Integer [^Integer n]
       (if (= n 0)
         1
         (* n (factorial (- n 1)))))
