99fea324f73e1e055f29d107d7c9a8279593cdb2
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-03-13 13:02:43 +0700

    read from stream

--- src/main/java/ru/nsu/valikov/Main.java
+++ src/main/java/ru/nsu/valikov/Main.java
@@ -7,17 +7,17 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import ru.nsu.valikov.generators.TranslationVisitor;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class Main {
 
-    public static void main(String[] args) {
-        String expression = """
-            (ns factorial)
-            (defn factorial ^Integer [^Integer n]
-                  (if (= n 0)
-                    1
-                    (* n (factorial (- n 1)))))
-            """;
-        ClojureLexer lexer = new ClojureLexer(CharStreams.fromString(expression));
+    public static void main(String[] args) throws IOException {
+        var file = "samples/factorial.clj";
+
+        var stream = CharStreams.fromStream(Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(file)));
+
+        ClojureLexer lexer = new ClojureLexer(stream);
         ClojureParser parser = new ClojureParser(new CommonTokenStream(lexer));
         ParseTree tree = parser.program();
         var res = new TranslationVisitor().visit(tree);
