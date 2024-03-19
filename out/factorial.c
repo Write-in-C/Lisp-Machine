ffc86f13c37142477ac84e919b0b856a2a231fe6
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-03-19 12:57:32 +0700

    move includes to separate file

--- out/factorial.c
+++ out/factorial.c
@@ -1,29 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int lisp_add_int(int a, int b) {
-    return a + b;
-}
-
-int lisp_neg_int(int a) {
-    return -a;
-}
-
-int lisp_sub_int(int a, int b) {
-    return a - b;
-}
-
-int lisp_mul_int(int a, int b) {
-    return a * b;
-}
-
-int lisp_div_int(int a, int b) {
-    return a / b;
-}
-
-int lisp_eq_int(int a, int b) {
-    return a == b;
-}
+#include "lisp_functions.h"
 
 int factorial(int n) {
     return lisp_eq_int(n, 0) ? 1 : lisp_mul_int(n, factorial(lisp_sub_int(n, 1)));
