87c5e491762324ba0d7323c60cc692d6f46a89d7
Nikolay <58514394+nocarend@users.noreply.github.com>
2024-03-25 00:03:10 +0700

    no types here thank you denis miginsky

--- src/main/java/ru/nsu/valikov/compiler/Scopes.java
+++ src/main/java/ru/nsu/valikov/compiler/Scopes.java
@@ -9,83 +9,35 @@ import ru.nsu.valikov.generators.Expression.TYPE;
 @UtilityClass
 public class Scopes {
 
-    private static final Deque<Map<String, Expression>> scopes = new ArrayDeque<>();
+    private static final Deque<Map<String, String>> scopes = new ArrayDeque<>();
+
+    public static final Map<String, List<Expression>> functions = new HashMap<>();
 
     public void createNew() {
         scopes.add(new HashMap<>());
-//        System.out.println("created" + scopes);
     }
 
     public boolean contains(String name) {
         return scopes.stream().anyMatch(i -> i.containsKey(name));
     }
 
-    public void defineNewVariable(String name, Expression var) {
-        if (contains(name) && getType(name) != var.type) {
-            throw new RuntimeException(
-                    ("""
-                            Duplicated variable names in intersected scopes.\s
-                            Try to use different name.\s
-                            Get: %s""").formatted(
-                            name));
-        }
-        scopes.getLast().put(name, var);
+    public void defineNewVariable(String name, String alias) {
+        scopes.getLast().put(name, alias);
     }
 
-    public int getIntValue(String name) {
+    public void setValue(String name, String value) {
         if (!contains(name)) {
-            throw new RuntimeException("No variable with such name: %s".formatted(name));
-        }
-        for (var scope : scopes) {
-            var value = scope.get(name);
-            if (value != null) {
-                if (value.type == TYPE.INT) {
-                    return Integer.parseInt(value.value);
-                }
-                throw new RuntimeException("Expected INT type, got %s".formatted(value.type));
-            }
-        }
-        throw new AssertionError("should not reach here");
-    }
-
-    public void setIntValue(String name, int value) {
-        if (!contains(name)) {
-            defineNewVariable(name, new Expression(TYPE.INT, String.valueOf(value)));
+            defineNewVariable(name, value);
             return;
-        }
-        for (var scope : scopes) {
-            var val = scope.get(name);
-            if (val != null) {
-                if (val.type == TYPE.INT) {
-                    scope.put(name, new Expression(TYPE.INT, String.valueOf(value)));
-                    return;
-                }
-                throw new RuntimeException(
-                        "Cannot assign INT to not INT variable: %s %s".formatted(name, val.type));
-            }
         }
         throw new AssertionError("should not reach here");
     }
 
     public void deleteLast() {
         scopes.pollLast();
-//        System.out.println("delete " + scopes);
     }
 
-    public TYPE getType(String name) {
-        if (!contains(name)) {
-            throw new RuntimeException("No variable with such name : %s".formatted(name));
-        }
-        for (var scope : scopes) {
-            var value = scope.get(name);
-            if (value != null) {
-                return value.type;
-            }
-        }
-        throw new AssertionError("Should not reach here");
-    }
-
-    public Expression get(String name) {
+    public String get(String name) {
         return scopes.stream()
                 .map(i -> i.get(name))
                 .filter(Objects::nonNull)
